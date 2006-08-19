b11cb7a6ee1e4d1c8a9a0fb324771ad54f9f84b1
Sharon Rosner <ciconia@gmail.com>
2006-08-19 05:56:04 +0000

    Added unit tests for static server.

--- lib/serverside/static.rb
+++ lib/serverside/static.rb
@@ -14,31 +14,50 @@ module ServerSide
       end
     end
     
-    class Const
+    module Const
       ETag = 'ETag'.freeze
       ETagFormat = '%x:%x:%x'.inspect.freeze
       CacheControl = 'Cache-Control'.freeze
-      StaticAge = (86400 * 30).freeze
-      StaticMaxAge = "max-age=#{StaticAge}".freeze
+      MaxAge = "max-age=#{86400 * 30}".freeze
       IfNoneMatch = 'If-None-Match'.freeze
-      NotModifiedClose = "HTTP/1.1 304 Not Modified\r\nConnection: close\r\nContent-Length: 0\r\nETag: %s\r\nCache-Control: max-age=#{StaticAge}\r\n\r\n".freeze
-      NotModifiedPersist = "HTTP/1.1 304 Not Modified\r\nContent-Length: 0\r\nETag: %s\r\nCache-Control: max-age=#{StaticAge}\r\n\r\n".freeze
+      NotModifiedClose = "HTTP/1.1 304 Not Modified\r\nConnection: close\r\nContent-Length: 0\r\nETag: %s\r\nCache-Control: #{MaxAge}\r\n\r\n".freeze
+      NotModifiedPersist = "HTTP/1.1 304 Not Modified\r\nContent-Length: 0\r\nETag: %s\r\nCache-Control: #{MaxAge}\r\n\r\n".freeze
       TextPlain = 'text/plain'.freeze
     end
+    
+    @@mime_types = Hash.new {|h, k| ServerSide::StaticFiles::Const::TextPlain}
+    @@mime_types.merge!({
+      '.html'.freeze => 'text/html'.freeze,
+      '.css'.freeze => 'text/css'.freeze,
+      '.js'.freeze => 'text/javascript'.freeze,
+
+      '.gif'.freeze => 'image/gif'.freeze,
+      '.jpg'.freeze => 'image/jpeg'.freeze,
+      '.jpeg'.freeze => 'image/jpeg'.freeze,
+      '.png'.freeze => 'image/png'.freeze
+    })
+    
+    @@static_files = {}
     
     def serve_file(fn)
       stat = File.stat(fn)
       etag = (Const::ETagFormat % [stat.mtime.to_i, stat.size, stat.ino]).freeze
       unless etag == @headers[Const::IfNoneMatch]
-        content = Cache.recall(fn, etag) || Cache.store(fn, etag, IO.read(fn).freeze)
-        send_response(200, $mime_types[File.extname(fn)], content, stat.size, 
-          {Const::ETag => etag, Const::CacheControl => Const::StaticMaxAge})
+        if @@static_files[fn] && (@@static_files[fn][0] == etag)
+          content = @@static_files[fn][1]
+        else
+          content = IO.read(fn).freeze
+          @@static_files[fn] = [etag.freeze, content]
+        end
+        
+        send_response(200, @@mime_types[File.extname(fn)], content, stat.size, 
+          {Const::ETag => etag, Const::CacheControl => Const::MaxAge})
       else
         @conn << ((@persistent ? Const::NotModifiedPersist : 
-          Const::NotModifiedClose) % tag)
+          Const::NotModifiedClose) % etag)
       end
     rescue => e
-      send_response(404, Const::TextHtml, 'Error reading file.')
+      send_response(404, Const::TextPlain, 'Error reading file.')
     end
   end
   
@@ -52,7 +71,7 @@ module ServerSide
       include StaticFiles
       
       def respond
-        fn = Const::WD/@path
+        fn = './%s' % @path
         if File.file?(fn)
           serve_file(fn)
         else
@@ -63,14 +82,3 @@ module ServerSide
   end
 end
 
-$mime_types = Hash.new {|h, k| ServerSide::StaticFiles::Const::TextPlain}
-$mime_types.merge!({
-  '.html'.freeze => 'text/html'.freeze,
-  '.css'.freeze => 'text/css'.freeze,
-  '.js'.freeze => 'text/javascript'.freeze,
-
-  '.gif'.freeze => 'image/gif'.freeze,
-  '.jpg'.freeze => 'image/jpeg'.freeze,
-  '.jpeg'.freeze => 'image/jpeg'.freeze,
-  '.png'.freeze => 'image/png'.freeze
-})
---
+++ test/unit/static_test.rb
@@ -0,0 +1,126 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'stringio'
+require 'fileutils'
+
+class ConnectionTest < Test::Unit::TestCase
+  class Dummy < ServerSide::Connection::Static
+    def self.static_files
+      @@static_files
+    end
+    
+    def self.mime_types
+      @@mime_types
+    end
+    
+    attr_accessor :conn, :path, :headers
+    
+    def initialize
+      @headers = {}
+    end
+  end
+  
+  def test_cache
+    assert_kind_of Hash, Dummy.static_files
+    Dummy.static_files.clear
+    c = Dummy.new
+    c.conn = StringIO.new
+    assert_nil Dummy.static_files[__FILE__]
+    c.serve_file(__FILE__)
+    cache = Dummy.static_files[__FILE__]
+    assert_not_nil cache
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::StaticFiles::Const::ETagFormat % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    assert_equal etag, cache[0]
+    assert_equal IO.read(__FILE__), cache[1]
+  end
+  
+  def test_mime_types
+    assert_kind_of Hash, Dummy.mime_types
+    assert_equal 'text/plain', Dummy.mime_types['.rb']
+    assert_equal 'text/plain', Dummy.mime_types['.invalid']
+    assert_equal 'text/html', Dummy.mime_types['.html']
+    assert_equal 'text/css', Dummy.mime_types['.css']
+    assert_equal 'text/javascript', Dummy.mime_types['.js']
+    assert_equal 'image/gif', Dummy.mime_types['.gif']
+    assert_equal 'image/jpeg', Dummy.mime_types['.jpg']
+    assert_equal 'image/jpeg', Dummy.mime_types['.jpeg']
+    assert_equal 'image/png', Dummy.mime_types['.png']
+  end
+  
+  def test_serve_file_normal
+    c = Dummy.new
+    c.conn = StringIO.new
+    c.serve_file(__FILE__)
+    c.conn.rewind
+    resp = c.conn.read
+    
+    assert_equal '200', /HTTP\/1.1\s(.*)\r\n/.match(resp)[1]
+    fc = IO.read(__FILE__)
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::StaticFiles::Const::ETagFormat % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    assert_equal etag, /ETag:\s(.*)\r\n/.match(resp)[1]
+    assert_equal ServerSide::StaticFiles::Const::MaxAge,
+      /Cache-Control:\s(.*)\r\n/.match(resp)[1]
+    assert_equal stat.size.to_s,
+      /Content-Length:\s(.*)\r\n/.match(resp)[1]
+  end
+  
+  def test_serve_file_etags
+    c = Dummy.new
+    c.conn = StringIO.new
+    Dummy.static_files.clear
+    c.serve_file(__FILE__)
+    c.conn.rewind
+    resp = c.conn.read
+    
+    # normal response
+    assert_equal '200', /HTTP\/1.1\s(.*)\r\n/.match(resp)[1]
+    fc = IO.read(__FILE__)
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::StaticFiles::Const::ETagFormat % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    assert_equal etag, /ETag:\s(.*)\r\n/.match(resp)[1]
+    assert_equal ServerSide::StaticFiles::Const::MaxAge,
+      /Cache-Control:\s(.*)\r\n/.match(resp)[1]
+    assert_equal stat.size.to_s,
+      /Content-Length:\s(.*)\r\n/.match(resp)[1]
+      
+    c.conn = StringIO.new
+    c.headers[ServerSide::StaticFiles::Const::IfNoneMatch] = etag
+    c.serve_file(__FILE__)
+    c.conn.rewind
+    resp = c.conn.read
+    
+    # not modified response
+    assert_equal '304 Not Modified', /HTTP\/1.1\s(.*)\r\n/.match(resp)[1]
+    fc = IO.read(__FILE__)
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::StaticFiles::Const::ETagFormat % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    assert_equal etag, /ETag:\s(.*)\r\n/.match(resp)[1]
+    assert_equal ServerSide::StaticFiles::Const::MaxAge,
+      /Cache-Control:\s(.*)\r\n/.match(resp)[1]
+    assert_equal '0', /Content-Length:\s(.*)\r\n/.match(resp)[1]
+    
+    FileUtils.touch(__FILE__)
+    c.conn = StringIO.new
+    c.headers[ServerSide::StaticFiles::Const::IfNoneMatch] = etag
+    c.serve_file(__FILE__)
+    c.conn.rewind
+    resp = c.conn.read
+    
+    # modified response
+    assert_equal '200', /HTTP\/1.1\s(.*)\r\n/.match(resp)[1]
+    fc = IO.read(__FILE__)
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::StaticFiles::Const::ETagFormat % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    assert_equal etag, /ETag:\s(.*)\r\n/.match(resp)[1]
+    assert_equal ServerSide::StaticFiles::Const::MaxAge,
+      /Cache-Control:\s(.*)\r\n/.match(resp)[1]
+    assert_equal stat.size.to_s,
+      /Content-Length:\s(.*)\r\n/.match(resp)[1]
+  end
+end
