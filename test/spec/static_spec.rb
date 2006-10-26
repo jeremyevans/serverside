3d5050297067b43069f966e25a2460e1030857d7
Sharon Rosner <ciconia@gmail.com>
2006-10-26 10:51:07 +0000

    Wrote Static spec.

---
+++ test/spec/static_spec.rb
@@ -0,0 +1,195 @@
+require File.join(File.dirname(__FILE__), '../../lib/serverside')
+require 'stringio'
+require 'fileutils'
+
+class Dummy < ServerSide::HTTP::Request
+  def self.static_files
+    @@static_files
+  end
+  
+  def self.mime_types
+    @@mime_types
+  end
+  
+  attr_accessor :path, :socket, :headers
+  
+  def initialize
+    super(nil)
+    @headers = {}
+  end
+end
+
+include ServerSide
+
+context "Static.static_files" do
+  specify "Should cache served files, along with their etag" do
+    Dummy.static_files.should_be_a_kind_of Hash
+    Dummy.static_files.clear
+    c = Dummy.new
+    c.socket = StringIO.new
+    Dummy.static_files[__FILE__].should_be_nil
+    c.serve_file(__FILE__)
+    cache = Dummy.static_files[__FILE__]
+    cache.should_not_be_nil
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::Static::ETAG_FORMAT % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    cache[0].should_equal etag
+    cache[1].should_equal IO.read(__FILE__)
+  end
+end
+
+context "Static.mime_types" do
+  specify "should be a hash" do
+    Dummy.mime_types.should_be_a_kind_of Hash
+  end
+  
+  specify "should return text/plain as the default mime type" do
+    Dummy.mime_types['.rb'].should_equal 'text/plain'
+    Dummy.mime_types['.invalid'].should_equal 'text/plain'
+  end
+  
+  specify "should return the correct mime type for common files" do
+    Dummy.mime_types['.html'].should_equal 'text/html' 
+    Dummy.mime_types['.css'].should_equal 'text/css' 
+    Dummy.mime_types['.js'].should_equal 'text/javascript' 
+    Dummy.mime_types['.gif'].should_equal 'image/gif' 
+    Dummy.mime_types['.jpg'].should_equal 'image/jpeg' 
+    Dummy.mime_types['.jpeg'].should_equal 'image/jpeg' 
+    Dummy.mime_types['.png'].should_equal 'image/png'
+  end
+end
+
+context "Static.serve_file" do
+  specify "should render correctly with file content, etag and size" do
+    c = Dummy.new
+    c.socket = StringIO.new
+    c.serve_file(__FILE__)
+    c.socket.rewind
+    resp = c.socket.read
+    
+    resp.should_match /HTTP\/1.1\s200(.*)\r\n/
+    fc = IO.read(__FILE__)
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::Static::ETAG_FORMAT % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    resp.should_match /ETag:\s"#{etag}"\r\n/
+    resp.should_match /Cache-Control:\smax-age=#{ServerSide::HTTP::Caching::DEFAULT_MAX_AGE}\r\n/
+    resp.should_match /Content-Length:\s#{stat.size.to_s}\r\n/
+  end
+  
+  specify "should send a not modified response only if a correct etag is specified in the request" do
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::Static::ETAG_FORMAT % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+
+    # normal response
+    c = Dummy.new
+    c.socket = StringIO.new
+    Dummy.static_files.clear
+    c.serve_file(__FILE__)
+    c.socket.rewind
+    resp = c.socket.read
+    
+    resp.should_match /HTTP\/1.1\s200(.*)\r\n/
+    resp.should_match /ETag:\s"#{etag}"\r\n/
+    resp.should_match /Cache-Control:\smax-age=#{ServerSide::HTTP::Caching::DEFAULT_MAX_AGE}\r\n/
+    resp.should_match /Content-Length:\s#{stat.size.to_s}\r\n/
+      
+    # normal response (invalid etag)
+    c = Dummy.new
+    c.socket = StringIO.new
+    c.headers[ServerSide::Static::IF_NONE_MATCH] = "\"xxx-yyy\""
+    Dummy.static_files.clear
+    c.serve_file(__FILE__)
+    c.socket.rewind
+    resp = c.socket.read
+    
+    resp.should_match /HTTP\/1.1\s200(.*)\r\n/
+    resp.should_match /ETag:\s"#{etag}"\r\n/
+    resp.should_match /Cache-Control:\smax-age=#{ServerSide::HTTP::Caching::DEFAULT_MAX_AGE}\r\n/
+    resp.should_match /Content-Length:\s#{stat.size.to_s}\r\n/
+    
+    # not modified (etag specified)
+    c.socket = StringIO.new
+    c.headers[ServerSide::Static::IF_NONE_MATCH] = "\"#{etag}\""
+    c.serve_file(__FILE__)
+    c.socket.rewind
+    resp = c.socket.read
+
+    resp.should_match /HTTP\/1.1\s304(.*)\r\n/
+    resp.should_match /ETag:\s"#{etag}"\r\n/
+    resp.should_match /Cache-Control:\smax-age=#{ServerSide::HTTP::Caching::DEFAULT_MAX_AGE}\r\n/
+
+    # modified response (file stamp changed)
+    FileUtils.touch(__FILE__)
+    c.socket = StringIO.new
+    c.headers[ServerSide::Static::IF_NONE_MATCH] = etag
+    c.serve_file(__FILE__)
+    c.socket.rewind
+    resp = c.socket.read
+    
+    resp.should_match /HTTP\/1.1\s200(.*)\r\n/
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::Static::ETAG_FORMAT % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    resp.should_match /ETag:\s"#{etag}"\r\n/
+    resp.should_match /Cache-Control:\smax-age=#{ServerSide::HTTP::Caching::DEFAULT_MAX_AGE}\r\n/
+    resp.should_match /Content-Length:\s#{stat.size.to_s}\r\n/
+  end
+end
+
+context "Static.serve_dir" do
+  specify "should render a directory with all its entries" do
+    dir = File.dirname(__FILE__)
+  
+    c = Dummy.new
+    c.socket = StringIO.new
+    Dummy.static_files.clear
+    c.path = dir
+    c.serve_dir(dir)
+    c.socket.rewind
+    resp = c.socket.read
+    
+    Dir.entries(dir).each do |fn|
+      next if fn =~ /^\./
+      resp.should_match /<a href="#{dir/fn}">(#{fn})<\/a>/
+    end
+  end
+end
+
+context "Static.serve_static" do
+  specify "should serve directories" do
+    dir = File.dirname(__FILE__)
+  
+    c = Dummy.new
+    c.socket = StringIO.new
+    Dummy.static_files.clear
+    c.path = dir
+    c.serve_static(dir)
+    c.socket.rewind
+    resp = c.socket.read
+    
+    Dir.entries(dir).each do |fn|
+      next if fn =~ /^\./
+      resp.should_match /<a href="#{dir/fn}">(#{fn})<\/a>/
+    end
+  end
+  
+  specify "should serve files" do
+    c = Dummy.new
+    c.socket = StringIO.new
+    c.serve_file(__FILE__)
+    c.socket.rewind
+    resp = c.socket.read
+    
+    # normal response
+    resp.should_match /HTTP\/1.1\s200(.*)\r\n/
+    stat = File.stat(__FILE__)
+    etag = (ServerSide::Static::ETAG_FORMAT % 
+      [stat.mtime.to_i, stat.size, stat.ino])
+    resp.should_match /ETag:\s"#{etag}"\r\n/
+    resp.should_match /Cache-Control:\smax-age=#{ServerSide::HTTP::Caching::DEFAULT_MAX_AGE}\r\n/
+    resp.should_match /Content-Length:\s#{stat.size.to_s}\r\n/
+  end
+end
