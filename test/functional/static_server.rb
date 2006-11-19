9c78be140877340d7787afe6e6f579e0982ff7e9
Sharon Rosner <ciconia@gmail.com>
2006-11-19 09:42:23 +0000

    Removed unit and functional tests.

--- test/functional/static_server.rb
+++
@@ -1,7 +0,0 @@
-require 'lib/serverside'
-
-trap('INT') {exit}
-include ServerSide
-puts "Serving on port 8000..."
-Router.default_route {serve_static('.'/@path)}
-HTTP::Server.new('0.0.0.0', 8000, Router).start
