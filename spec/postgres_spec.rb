c9c036badae37209fd4ffd56b57efa5f872de6c8
Sharon Rosner <ciconia@gmail.com>
2007-03-04 06:24:46 +0000

    Removed ORM code, which has been factored into a separate library: sequel.

--- spec/postgres_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.join(File.dirname(__FILE__), '../lib/serverside')
-require File.join(File.dirname(__FILE__), '../lib/serverside/db/postgres')
-
-context "A new Postgres::Database" do
-end
-
